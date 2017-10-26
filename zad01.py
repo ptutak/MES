3c0a6747259def9bda6a8dfed753c3af83952b14
ptutak <p.tutak@outlook.com>
2017-10-26 19:08:19 +0200

    poprawki

--- zad01.py
+++ zad01.py
@@ -4,31 +4,20 @@ Created on Sat Oct 21 11:01:53 2017
 
 @author: PiotrTutak
 """
-import numpy
-import scipy
+import numpy as np
+import scipy.linalg as lg
 
-print("Podaj L1,L2,L3,L4")
+print("Podaj L1 L2 L3 L4")
 
 L=[float(x) for x in input().strip().split()]
 
-print('Podaj k,S,q,alfa,tInf')
+print('Podaj k S q alfa tInf')
 
 k,S,q,alfa,tInf=(float(x) for x in input().strip().split())
-"""
-print("k=",end='')
-k=float(input().strip())
-print("S=",end='')
-S=float(input().strip())
-print("q=",end='')
-q=float(input().strip())
-print("alfa=",end='')
-alfa=float(input().strip())
-print("tInf=",end='')
-tInf=float(input().strip())
-"""
+
 C=[k*S/l for l in L]
 
-A=numpy.array([
+A=np.array([
    [C[0],-C[0],0,0,0],
    [-C[0],C[0]+C[1],-C[1],0,0],
    [0,-C[1],C[1]+C[2],-C[2],0],
@@ -36,7 +25,7 @@ A=numpy.array([
    [0,0,0,-C[3],C[3]+alfa*S]
    ])
 
-P=numpy.array([
+P=np.array([
         q*S,
         0,
         0,
@@ -46,5 +35,6 @@ P=numpy.array([
 
 P=-P
 
-t=numpy.linalg.solve(A,P)
+#t=np.linalg.solve(A,P)
+t=lg.solve(A,P)
 print(t)
