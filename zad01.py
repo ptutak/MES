dc0579bd5d63e7851a93be888e87f72f2fc7a8c2
Piotr Tutak <p.tutak@outlook.com>
2017-10-27 07:50:44 +0200

    Update zad01.py

--- zad01.py
+++ zad01.py
@@ -6,7 +6,7 @@ Created on Sat Oct 21 11:01:53 2017
 """
 import numpy as np
 import scipy.linalg as lg
-
+import matplotlib.pyplot as plt
 print("Podaj L1 L2 L3 L4")
 
 L=[float(x) for x in input().strip().split()]
@@ -16,7 +16,8 @@ print('Podaj k S q alfa tInf')
 k,S,q,alfa,tInf=(float(x) for x in input().strip().split())
 
 C=[k*S/l for l in L]
-
+L=[sum(L[:i] for i in range(1,len(L)+1)]
+       
 A=np.array([
    [C[0],-C[0],0,0,0],
    [-C[0],C[0]+C[1],-C[1],0,0],
@@ -38,3 +39,5 @@ P=-P
 #t=np.linalg.solve(A,P)
 t=lg.solve(A,P)
 print(t)
+plt.plot(L,t)
+plt.show()
