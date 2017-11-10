a8eb389ca323446d2fb880717e3a7c710e245c30
Piotr Tutak <p.tutak@outlook.com>
2017-11-10 11:10:21 +0100

    calka done

--- zad02.py
+++ zad02.py
@@ -5,6 +5,8 @@ Created on Thu Nov  9 16:58:46 2017
 @author: PiotrTutak
 """
 
+import numpy as np
+
 def f(x,y):
     return 2*(x**2)*(y**2)+6*x+5
 
@@ -26,4 +28,16 @@ def calkaPodwojna(f,ax,bx,ay,by,n,m):
     calka=calka*dx*dy*0.25
     return calka
 
+
+def calkaGauss(f, kwadratury, wagi):
+    calka=0.0
+    
+    for i in range(len(kwadratury)):
+        for j in range(len(kwadratury)):
+            calka+=f(kwadratury[i],kwadratury[j])*wagi[i]*wagi[j]
+            
+    return calka
+
+
 print(calkaPodwojna(f,-1.0,1.0,-1.0,1.0,100,100))
+print(calkaGauss(f,[-np.sqrt(3)/3,np.sqrt(3)/3],[1.0,1.0]))
